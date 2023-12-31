82df1aa10f738baf82c48a7371ffd3315e7f4e0f
smccdsebrianne <fergusons@smccd.edu>
2023-12-30 21:30:50 -0800

    instagram link and updated pictures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,14 +48,19 @@ const Header = () => {
 const Footer = () => {
   return (
     <footer className="flex bg-emerald-100 h-32 justify-center items-center flex-col">
+      <div className="pt-2">
+        <Link href="https://instagram.com/emeraldbakeryca">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="32"
+            width="30"
+            viewBox="0 0 448 512"
+          >
+            <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+          </svg>
+        </Link>
+      </div>
       <h1 className="text-black mb-3">© 2023 Emerald Bakery</h1>
-      <Image
-        className="mx-auto"
-        src="/logo.png"
-        width={50}
-        height={50}
-        alt="Emerald Logo"
-      />
     </footer>
   );
 };
@@ -119,7 +124,7 @@ export default function Home() {
             <div className="bg-white flex flex-col items-center rounded-md">
               <div className="h-40 w-full rounded flex items-center justify-center">
                 <Image
-                  src="/sfcookie.png"
+                  src="/49ers.jpg"
                   width={525}
                   height={788}
                   alt="Emerald Logo"
